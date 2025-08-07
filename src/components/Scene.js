68172d68a27f49a1726ada595ae8a40d1f8815f9
Diacoma Alicia <aliciadiacoma29@gmail.com>
2025-08-08 09:44:36 +1100

    Ajout postprocessing et ajout controller LEVA

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import Model from './Model';
+import { EffectComposer, Bloom, DepthOfField, Vignette, Noise } from '@react-three/postprocessing';
+import { useControls } from 'leva';
 import AnimatedModel from './AnimatedModel';
 
 function Scene() {
+  // Contrôles Leva pour ajuster les effets en temps réel
+  const { 
+    bloomIntensity,
+    bloomRadius,
+    vignetteIntensity,
+    depthOfFieldFocus,
+    depthOfFieldBokeh,
+    noiseOpacity
+  } = useControls('Post-Processing', {
+    bloomIntensity: { value: 0.5, min: 0, max: 2, step: 0.1 },
+    bloomRadius: { value: 0.8, min: 0, max: 2, step: 0.1 },
+    vignetteIntensity: { value: 0.3, min: 0, max: 1, step: 0.1 },
+    depthOfFieldFocus: { value: 0.1, min: 0, max: 1, step: 0.01 },
+    depthOfFieldBokeh: { value: 2.0, min: 0, max: 10, step: 0.1 },
+    noiseOpacity: { value: 0.02, min: 0, max: 0.1, step: 0.01 }
+  });
+
   return (
     <>
       {/* Éclairage */}
@@ -26,7 +45,7 @@ function Scene() {
       {/* Sol */}
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]} receiveShadow>
         <planeGeometry args={[200, 200]} />
-        <meshStandardMaterial color="#b8f2e6" />
+        <meshStandardMaterial color="#2b2d42" />
       </mesh>
 
       {/* Modèles statiques */}
@@ -67,9 +86,39 @@ function Scene() {
         path="/models/Pimelea Suaveolens.glb" 
         position={[-8, 0, -6]} 
         scale={5}
-        // Teste ajout de rotation 
         rotation={[0, Math.PI / 2, 0]}
       />
+
+      {/* Effets Post-Processing */}
+      <EffectComposer>
+        {/* Effet Bloom - Lueur lumineuse */}
+        <Bloom 
+          intensity={bloomIntensity}
+          radius={bloomRadius}
+          luminanceThreshold={0.9}
+          luminanceSmoothing={0.025}
+        />
+        
+        {/* Effet Depth of Field - Flou de profondeur */}
+        <DepthOfField
+          focusDistance={depthOfFieldFocus}
+          focalLength={0.02}
+          bokehScale={depthOfFieldBokeh}
+          height={480}
+        />
+        
+        {/* Effet Vignette - Assombrissement des bords */}
+        <Vignette
+          eskil={false}
+          offset={0.1}
+          darkness={vignetteIntensity}
+        />
+        
+        {/* Effet Noise - Grain cinématographique */}
+        <Noise 
+          opacity={noiseOpacity}
+        />
+      </EffectComposer>
     </>
   );
 }
